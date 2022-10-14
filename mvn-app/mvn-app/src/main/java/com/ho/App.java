f2db401a9e689b02b5452c3aefb2602a3e498301
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-10-14 08:59:20 +0000

    [New] make getMessage

--- mvn-app/mvn-app/src/main/java/com/ho/App.java
+++ mvn-app/mvn-app/src/main/java/com/ho/App.java
@@ -4,10 +4,19 @@ package com.ho;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public String getMessage(String name) {
+        return "Hi " + name + ". Welcome to Maven World!";
+    }
+
+    public void printMessage(String name) {
+        System.out.println("\n **** app class ****");
+        System.out.println(this.getMessage(name));
+        System.out.println("\n **** app class ****");
+
     }
 }

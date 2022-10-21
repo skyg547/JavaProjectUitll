2f47004dcae4e024d738534108e49e633face6ea
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-10-21 09:01:10 +0000

    [gradle] app class

---
+++ gradle/gradle-app/src/main/java/com/ho/gradle/App.java
@@ -0,0 +1,31 @@
+package com.ho.gradle;
+
+public class App{
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        System.out.println("===========App class ======")
+
+        app.showMessage("tard");
+        System.out.println("===========App class ======")
+    }
+
+    private boolean showMessage(String name) {
+        try {
+            System.out.println(this.getMessage(name));
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("error")
+
+        } finally{
+            return true;
+        }
+        
+    }
+
+    public String getMessage(String name) {
+        return "hi" + name
+        
+    }
+}
